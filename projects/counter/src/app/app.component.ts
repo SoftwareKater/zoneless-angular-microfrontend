7c91122d20734c0c9aad12e4c8109497f69f39ce
SoftwareKater <softwarekater@gmail.com>
2021-08-24 11:10:50 +0200

    add detectChanges call to onClick method

--- projects/counter/src/app/app.component.ts
+++ projects/counter/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +10,11 @@ export class AppComponent {
 
   public state = { counter: 0 };
 
+  constructor(private readonly cdr: ChangeDetectorRef) { }
+
   public onClick(): void {
     this.state.counter += 1;
     console.log('New counter value: ', this.state.counter);
+    this.cdr.detectChanges();
   }
 }
