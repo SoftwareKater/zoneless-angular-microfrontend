e74dceb2b3bc3560d35402f48817847c6295eb1f
SoftwareKater <softwarekater@gmail.com>
2021-08-24 11:03:17 +0200

    unit test onClick method

--- projects/counter/src/app/app.component.spec.ts
+++ projects/counter/src/app/app.component.spec.ts
@@ -22,10 +22,13 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('counter');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('counter app is running!');
-  });
+  describe('onClick', () => {
+    it('updates the state', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.state.counter).toBe(0);
+      app.onClick();
+      expect(app.state.counter).toBe(1);
+    });
+  })
 });
